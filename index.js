d88f6aafea82448fb94b5c219aac203cd92aadef
JoseOropeza-uru <joseoropeza@uru.edu>
2019-02-19 18:42:20 -0400

    Client change, rooms update

--- index.js
+++ index.js
@@ -2,25 +2,49 @@ const express = require('express');
 const app = express();
 const config = require('./helpers/config');
 
-app.use('/views', express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
-
-app.get('/',(req,res)=>{
-	// res.sendFile(__dirname + '/index.html');
-	res.redirect('views/index.html');
-});
-
 const server = app.listen(config.port);
 const io = require('socket.io').listen(server);
-io.sockets.on('connection', function(socket){
-	io.emit('hello');
-	socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-	});
-	socket.on('disconnected',function(user){
-		socket.broadcast.emit('disconnected',user);
-	});
-  });
+let users ={};
 
+io.sockets.on('connection', socket=>{
 
-  
+	socket.on("disconnect", ()=> {
+		let index = users[socket.room].findIndex(el => el === socket.nickname);
+		users[socket.room].splice(index, 1);
+
+		io.sockets.in(socket.room).emit("in-chat", {
+			nickname: socket.nickname,
+			event: "left",
+			room: socket.room,
+			users: users[socket.room]
+		});
+	});
+	socket.on("open-chat", chatInfo => {
+		console.log(chatInfo);
+		socket.nickname = chatInfo.nickname;
+		socket.room = chatInfo.room;
+		socket.join(chatInfo.room);
+
+		if (users.hasOwnProperty(chatInfo.room)) {
+			users[chatInfo.room].push(chatInfo.nickname);
+		} else {
+			users[chatInfo.room] = [];
+			users[chatInfo.room].push(chatInfo.nickname);
+		}
+
+		io.sockets.in(socket.room).emit("in-chat", {
+			nickname: chatInfo.nickname,
+			event: "joined",
+			room: chatInfo.room,
+			users: users[chatInfo.room]
+		});
+	});
+
+	socket.on("send-msg", message => {
+		io.sockets.in(socket.room).emit("message", {
+			text: message.text,
+			from: socket.nickname,
+			created: new Date()
+		});
+	});
+});
